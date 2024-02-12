1b426b0c546c3437677954c0162b93e47daf7e7a
Oleh Stolyar <stolyar.oleh@gmail.com>
2024-02-12 20:03:55 +0000

    Build with crane

--- flake.nix
+++ flake.nix
@@ -2,62 +2,54 @@
   description = "A devShell example";
 
   inputs = {
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     { self
+    , crane
+    , flake-utils
     , nixpkgs
     , rust-overlay
-    , flake-utils
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
 
         rust = pkgs.rust-bin.stable.latest;
+        rustToolchain = rust.default.override {
+          extensions = [
+            # include source for LSP
+            "rust-src"
+          ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-        rustPlatform =
-          let
-            rustVersion = rust.default.override {
-              # include source for LSP
-              extensions = [ "rust-src" "rustfmt" ];
-            };
-          in
-          pkgs.makeRustPlatform {
-            cargo = rustVersion;
-            rustc = rustVersion;
-          };
-
-        brag =
-          let
-            cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          in
-          rustPlatform.buildRustPackage {
-            pname = cargoToml.package.name;
-            version = cargoToml.package.version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
+        brag = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strictDeps = true;
+        };
       in
       {
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ brag ];
-          buildInputs = with pkgs; [
+        packages.default = brag;
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
             eza
             fd
             rust.rust-analyzer
           ];
         };
-
-        packages.default = brag;
       }
     );
 }
