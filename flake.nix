0a116a31ec6136804003ea5a22c2c5dc83f0605f
Oleh Stolyar <stolyar.oleh@gmail.com>
2024-02-12 20:14:15 +0000

    Add checks

--- flake.nix
+++ flake.nix
@@ -36,12 +36,42 @@
         };
         craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-        brag = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
-          strictDeps = true;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          srtictDeps = true;
         };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        brag = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
       {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit brag;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, resuing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          brag-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          # Check formatting
+          brag-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
         packages.default = brag;
         devShells.default = craneLib.devShell {
           packages = with pkgs; [
